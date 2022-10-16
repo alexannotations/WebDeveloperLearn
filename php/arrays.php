72cfc882398de9b0722c1c5eca2eb971c819e56d
alexannotations <69085537+alexannotations@users.noreply.github.com>
2022-10-16 03:32:32 -0500

    manipulacion de arrays

--- php/arrays.php
+++ php/arrays.php
@@ -1,6 +1,7 @@
 <?php
 /*
 https://www.php.net/manual/es/language.types.array.php
+https://www.php.net/manual/es/ref.array.php
 
 El operador => indica la relación asociativa entre una clave y un valor.
 La clave nos permite acceder a los valores almacenados en el arreglo.
@@ -15,6 +16,9 @@ echo "numero en arreglo: ".$arreglo[1] ."\n";
 echo "numero en arreglof: ".$arreglof[1] ."\n";
 
 # los arreglos asociativos son parecidos a las listas
+# Es el equivalente a los objetos JSON de JavaScript, 
+# sobre todo porque podemos verlos como una lista, 
+# y los subíndices son palabras.
 $edades=array(
     "Brenda"=> 18,
     "Magda"=> 22,
@@ -43,4 +47,19 @@ $personas=array(
 echo "La beca de Ana es: " .$personas["Ana"]["beca"]. 
         ". Su edad es: ".$personas["Ana"]["edad"]."\n";
 
+# Manipulación de arreglos
+echo count($edades) ."\n";  // cuenta cuantos valores tiene el array
+array_push($arreglo,44);    // agregamos un valor al array
+var_dump($arreglo); // observamos los valores del array
+
+$NotArray=0;
+var_dump(is_array($NotArray));  // checamos si es un array
+
+# convertir un string en un array, hay que indicar el separador y el string
+$listaFrutas="Sandia,Piña,Platano,Mango";
+$frutasArray=explode(",",$listaFrutas); // funcion explode()
+var_dump($listaFrutas);
+var_dump($frutasArray);
+$frutasString=implode(",",$frutasArray); // convierte a string un array
+var_dump($frutasString);
 ?>
