bfdad11cfd1818a5199029ead4f7922e8d9e34a5
alexannotations <69085537+alexannotations@users.noreply.github.com>
2022-10-16 00:36:12 -0500

    mas operadores

--- php/operadoresAritmeticos.php
+++ php/operadoresAritmeticos.php
@@ -1,14 +1,34 @@
 <?php
 /*
-Adición 	+
-Sustracción 	-
-Multiplicación 	*
-División 	/
-Módulo 	%
-Potenciación 	**
-Identidad 	Convierte un string a int o float, según sea el caso. 	+ 	+$a
-Negación 	Convierte un número a positivo o negativo. 	- 	-$a
+
+    Adición 	+
+    Sustracción 	-
+    Multiplicación 	*
+    División 	/
+    Módulo 	%
+    Potenciación 	**
+    Identidad 	Convierte un string a int o float, según sea el caso. 	+ 	+$a
+    Negación 	Convierte un número a positivo o negativo. 	- 	-$a
+
+    = 	Asignación
+    += 	Incremento especifico
+    ++ 	Incremento en uno
+    -= 	Decremento especifico
+    -- 	Decremento en uno
+    *= 	Multiplicación
+    /= 	División
+    .= 	Concatenación
+
 */
 
 echo (5*6) ." ". (80/4)."\n";
+
+# Operador de asignacion, =
+$year=18
+
+$contador=1;
+$contador++;        // $contador+=1;    $contador=$contador+1;
+
+$name="Usuario";
+$name.= " "."lastName";      // $name=$name ." "."lastName";
 
