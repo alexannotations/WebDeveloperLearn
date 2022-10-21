7def285026d588566b8823fe04468a9e7cc1471e
alexannotations <69085537+alexannotations@users.noreply.github.com>
2022-10-20 23:21:07 -0500

    operadores matematicos 12.2

--- php/calculadora.php
+++ php/calculadora.php
@@ -1,3 +1,10 @@
+<style>
+    .resultado{
+        color: #F00;
+        font-weight: bold;
+        font-size: 32px;
+    }
+</style>
 <?php
 //var_dump($_POST);
 print_r($_POST);
@@ -5,21 +12,33 @@ print_r($_POST);
         $numero1=$_POST["num1"];
         $numero2=$_POST["num2"];
         $operacion=$_POST["operacion"];
+        calcular($operacion);
+    }
 
-        if (!strcmp("Suma",$operacion)) {
-            echo "El resultado es: " .($numero1+$numero2);
+    // paso de parametros
+    function calcular($calculo){
+
+        global $numero1;
+        global $numero2;
+
+        if (!strcmp("Suma",$calculo)) {
+            echo "<p class='resultado'>El resultado es: " .($numero1+$numero2) ."</p>";
         }
-        if (!strcmp("Resta",$operacion)) {
-            echo "El resultado es: " .($numero1-$numero2);
+        if (!strcmp("Resta",$calculo)) {
+            $resultado=$numero1-$numero2;
+            echo "<p class='resultado'>El resultado es:  $resultado</p>";
         }
-        if (!strcmp("Multiplicacion",$operacion)) {
-            echo "El resultado es: " .($numero1*$numero2);
+        if (!strcmp("Multiplicacion",$calculo)) {
+            $resultado=$numero1*$numero2;
+            echo "<p class='resultado'>El resultado es:  $resultado</p>";
         }
-        if (!strcmp("Division",$operacion)) {
-            echo "El resultado es: " .($numero1/$numero2);
+        if (!strcmp("Division",$calculo)) {
+            $resultado=$numero1/$numero2;
+            echo "<p class='resultado'>El resultado es:  $resultado</p>";
         }
-        if (!strcmp("Modulo",$operacion)) {
-            echo "El resultado es: " .($numero1%$numero2);
+        if (!strcmp("Modulo",$calculo)) {
+            $resultado=$numero1%$numero2;
+            echo "<p class='resultado'>El resultado es:  $resultado</p>";
         }
     }
 ?>
