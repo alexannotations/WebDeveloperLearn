19d7cdd86b22fa220d2405c8dfd3dae440e00732
alexannotations <69085537+alexannotations@users.noreply.github.com>
2022-10-21 11:32:11 -0500

    estruc control blucle FOR

---
+++ php/ecFor.php
@@ -0,0 +1,19 @@
+<?php
+# estructura de control for
+# bucle
+#
+# https://www.php.net/manual/es/control-structures.for.php
+
+#  declarar múltiples variables en el ciclo for
+for ($i = 0, $j = 0; $i < 10; $i++, $j += 2) {
+    // Escribir todo nuestro código
+    echo "i = $i j = $j" . "\n";
+}
+
+for ($i=0; $i < 10; $i++) { 
+    # code...
+}
+
+echo "\n";
+
+?>
