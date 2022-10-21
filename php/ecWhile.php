d55cd3de24fb9a7989dad3fd1839b13a70c7b0ad
alexannotations <69085537+alexannotations@users.noreply.github.com>
2022-10-21 10:43:02 -0500

    estruc control blucle WHILE

---
+++ php/ecWhile.php
@@ -0,0 +1,16 @@
+<?php
+# estructura de control While
+# bucle
+#
+# https://www.php.net/manual/es/control-structures.while.php
+
+$contador = 1;
+
+while($contador <= 10) {
+    echo "Actualmente estamos en la iteración $contador \n";
+    $contador++;
+}
+
+echo "\n";
+
+?>
