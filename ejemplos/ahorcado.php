2d37b172921c95c483d3b90350043208840e190d
alexannotations <69085537+alexannotations@users.noreply.github.com>
2022-10-21 21:41:45 -0500

    juago ahorcado letra introducida

--- ejemplos/ahorcado.php
+++ ejemplos/ahorcado.php
@@ -22,7 +22,22 @@ echo $discovered_letters . "\n\n";
 $player_letter = readline("Escribe una letra: ");
 $player_letter = strtolower($player_letter);
 
-
+//  PHP8 verificar letra introducida (ahuja en un pajar)
+if (str_contains($choosen_word,$player_letter)) {
+    // $letter_position es una variable que no influye, pero es creada debido a la precedencia de php
+    // se usara mas adelante, devuelve la posicion de la letra que el usuario escribio, 
+    // se usa while para devolver todas las ocurrencias, devuelve false al no encontrar mas
+    $offset=0;
+    while ($letter_position=strpos($choosen_word,$player_letter,$offset)!==false) {
+        // un string es un array, al array ___ se colocara la letra indicada
+        $discovered_letters[$letter_position]=$player_letter;   // presenta error
+        $offset=$letter_position+1;
+    }
+} 
+else{
+    $attempts++;
+    echo "Letra incorrecta. Te quedan ". MAX_ATTEMPS-$attempts ." intentos.";
+}
 
 echo "\n";
 
