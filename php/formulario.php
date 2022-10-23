7fcf6b2b2ec3baa409a5975f195463dd83eeb9b5
Alexannotations <69085537+alexannotations@users.noreply.github.com>
2022-10-23 12:27:10 -0500

    mejorando el formulario

--- php/formulario.php
+++ php/formulario.php
@@ -2,25 +2,23 @@
 # https://www.php.net/manual/es/reserved.variables.request.php
 
 # ayuda a recibir los datos que envia el formulario en forma de un array
-print_r($_REQUEST);
+//print_r($_REQUEST);
 
 
 $words=array("Samantha","Sandra","Natalia","Veronica");
 # https://www.php.net/manual/es/function.str-shuffle.php
-$shuffle_words=array();
+
+
+$form="<form action='formulario_analisis.php'>";
 
 foreach ($words as $key => $name) {
-    $shuffle_words[$key]=str_shuffle($name);
-    echo $shuffle_words[$key].",";
+    $form.="
+    La palabra: " .str_shuffle($name) ." "."<input type='text' name='palabra".$key."'><br>";
 }
 
-echo "
-    <form action='formulario_analisis.php'>
-        <input type='text' name='palabra0'>
-        <input type='text' name='palabra1'>
-        <input type='text' name='palabra2'>
-        <button type='submit'>Enviar</button>
-    </form>
-";
+$button="\n\t<button type='submit'>Enviar</button>\n";
+$formclose="</form>";
+
+echo $form.$button.$formclose;
 
 ?>
