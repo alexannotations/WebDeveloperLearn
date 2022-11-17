4a2b04a765e90d28932f56e0cab095a022ac37a6
Alexannotations <69085537+alexannotations@users.noreply.github.com>
2022-11-16 19:56:10 -0600

    paso variables a javascript

---
+++ php/html_variables_javascript.php
@@ -0,0 +1,49 @@
+<?php  
+$usuarios=array(
+    array(
+        "id"=>0,
+        "username"=>"Sandra"
+    ),
+    array(
+        "id"=>1,
+        "username"=>"Samantha"
+    ),
+    array(
+        "id"=>2,
+        "username"=>"Sarahi"
+    )
+    );
+$edad_usuario=23;
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pasar variables de PHP a JavaScript</title>
+</head>
+<body>
+
+<!-- el script es javascript -->
+<script>
+    // convierte un string a JSON, convierte un arreglo a un stringJSON
+    let users = JSON.parse('<?= json_encode($usuarios) ?>');
+    //console.log(users);
+    let edadUsuario = <?= $edad_usuario ?>;
+</script>
+
+<section>
+    <h2>Pasar variables de Javascript a php</h2>
+    <p>Se puede hacer, pero tal vez necesite replantearse porque
+        se quiere hacer, ya que esto es tan simple como hacer una 
+        petición asincrona al servidor.
+    </p>
+    <p>Recuerda que PHP solo se ejecuta en tiempode carga, 
+        mientras la pagina es preprocesada, javascript se
+        ejecuta despues de que la pagina fue cargada.
+    </p>
+    
+</section>
+    
+</body>
+</html>
