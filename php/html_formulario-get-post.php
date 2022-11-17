73268eeef1fdf1950b3b13924c9d1392e7545c58
alexannotations <69085537+alexannotations@users.noreply.github.com>
2022-11-17 11:34:53 -0600

    formulario post

--- php/html_formulario-get-post.php
+++ php/html_formulario-get-post.php
@@ -3,10 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulario por GET</title>
+    <title>Formulario por GET y POST</title>
 </head>
 <body>
     <form action="htmlserverget-post.php" method="get">
+        <h2>Fromulario GET</h2>
         <!-- el atributo name al definirlo permite mandar los datos por el metodo GET -->
         <label for="nombre">Nombre:</label>
         <input type="text" name="name_user" id="nombre">
@@ -17,5 +18,17 @@
         <button type="submit">Mandar formulario</button>
     </form>
     
+    <form action="htmlserverget-post.php" method="post">
+        <h2>Fromulario POST</h2>
+        <!-- el atributo name al definirlo permite mandar los datos por el metodo POST -->
+        <label for="nombre">Nombre:</label>
+        <input type="text" name="name_user" id="nombre">
+
+        <label for="edadid">Edad:</label>
+        <input type="text" name="age_user" id="edadid">
+
+        <button type="submit">Mandar formulario</button>
+    </form>
+
 </body>
 </html>
--- php/htmlserverget-post.php
+++ php/htmlserverget-post.php
@@ -1,9 +1,19 @@
 <?php
-echo "<pre>";
+echo "<h2>GET</h2><pre>";
 echo  var_dump($_GET);
 echo "</pre>";
 
 $nombre = $_GET["name_user"];
 $edad = $_GET["age_user"];
+
+echo "El usuario $nombre tiene $edad años.";
+
+// --- --- --- --- --- --- 
+echo "<h2>POST</h2><pre>";
+echo  var_dump($_POST);
+echo "</pre>";
+
+$nombre = $_POST["name_user"];
+$edad = $_POST["age_user"];
 
 echo "El usuario $nombre tiene $edad años.";
