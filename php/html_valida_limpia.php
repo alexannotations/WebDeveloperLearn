e4cf6e5317554bc01e748e5934b0277a0590724c
Alexannotations <69085537+alexannotations@users.noreply.github.com>
2022-11-17 18:05:12 -0600

    formulario sanitizacion filter

--- php/html_valida_limpia.php
+++ php/html_valida_limpia.php
@@ -1,4 +1,7 @@
 <?php
+$username=null;
+$useremail=null;
+$userage=null;
 // Determina si una variable está definida y no es null
 echo "- isset name_user:".var_dump(isset($_POST["name_user"]))."<br>";
 // Determina si una variable está vacía
@@ -16,8 +19,22 @@ echo "- notempty form:".var_dump(!empty($_POST["form"]))."<br>";
 if ( isset($_POST["form"]) ) {
     echo "Toda la informacion del formulario fue mandada";
     echo "<pre>";
-    echo var_dump($_POST["form"]);
+    echo var_dump($_POST);
     echo "</pre>";
+    $username=$_POST["name_user"];
+    $useremail=$_POST["email_user"];
+    $userage=$_POST["age_user"];
+
+    // Sanitizacion de variables, debe realizarse con todas las variables
+    // Convierte todos los caracteres aplicables a entidades HTML
+    $htmlusername=htmlentities($username);
+    // escapar caracteres
+    // Escapa un string con barras invertidas '
+    $slashusername=addslashes($username);
+    // expresiones regulares: unicamente las letras "/\d/" numeros "/\D/"
+    $onlywords=preg_replace("/\d/","",$username);
+    // Filtra una variable con el filtro que se indique
+    $email=filter_var($email,FILTER_SANITIZE_EMAIL);
 }
 else {
     echo "No se mando ninguna informacion en el formulario";
@@ -28,7 +45,7 @@ else {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Validando el envío de un formulario</title>
+    <title>Validando el envío de un formulario y Sanitizacion</title>
 </head>
 <body>
     
@@ -36,10 +53,30 @@ else {
     <label for="nombre">Nombre</label>
     <input type="text" name="name_user" id="nombre">
 
+    <label for="emai">email</label>
+    <input type="email" name="email_user" id="email">
+
+    <label for="age">Nombre</label>
+    <input type="number" name="age_user" id="age">
+
     <!-- envia el encabezado de form sin datos. name_user si contiene datos al enviar 
         Se puede usar form para validar si todo el formulario se envio -->
     <button name="form" type="submit">Mandar formulario</button>
 </form>
-    
+
+<p>
+Se podria hackear el formulario si no se sanitiza lo introducido
+<b>&lt;div style="background-color: red;"&gt;hola te hakie&lt;/div&gt;</b>
+EL archivo permite inyectar codigo html, y se muestra como se limpia
+</p>
+
+<p>
+    User name: <?=$username?><br>
+    htmlentities(username): <?=$htmlusername?><br>
+    addslashes(username): <?=$slashusername?><br>
+    regex(username): <?=$onlywords?><br>
+    User email: <?=$useremail?><br>
+    User age: <?=$userage?><br>
+</p>
 </body>
 </html>
