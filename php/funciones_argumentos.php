466d08758207e195487384c1656cea70f658db58
alexannotations <69085537+alexannotations@users.noreply.github.com>
2022-12-01 10:26:26 -0600

    argumentos por defecto y obligatorios

---
+++ php/funciones_argumentos.php
@@ -0,0 +1,62 @@
+<?php
+/** Argumentos
+ * https://www.php.net/manual/es/functions.arguments.php
+ * 
+ */
+
+// Pasa un array como parámetro por defecto, observe que ya estan definidos los valores
+function sumar_edades($edades = array(13, 17, 35)) {
+    return array_sum($edades);
+  }
+  // Usa el array que se esta pasando
+  echo sumar_edades(array(5, 10, 15)) . PHP_EOL;    // PHP_EOL El símbolo 'Fin De Línea' correcto de la plataforma en uso. 
+/** Se puede crear una funcion donde se incluyan parametros por defecto y parametros obligatorios
+ * para esto siempre se deben indicar primero los parametros obligatorios, y despues los parametros por defecto
+ * 
+ * function hacerCalculo($n1, $n2=8){   #code  }
+ * */
+
+  /**
+   * Trailing commas 
+   * implementado en PHP 8
+   * Son comas que se pueden dejar al aire,
+   * observe la ultima coma al final de argumento
+   * Se usa para generar codigo dinamico, o bien solo por comodidad
+   */
+  function multiplicar(
+                        $n1 = 1, 
+                        $n2 = 2, 
+                        $n3 = 3,
+                    ) {
+    return $n1 * $n2 * $n3;
+  }
+  echo multiplicar() . PHP_EOL;
+  
+
+
+  class UnaClaseRandom {
+
+  }
+  class OtraClaseRandom {
+
+  }
+  
+  /** La palabra reservada class es usada para la resolución de nombres de clases. 
+   * Se puede obtener un string con el nombre completamente cualificado de 
+   * la clase ClassName utilizando ClassName::class. 
+   * Esto es particularmete útil con clases en espacios de nombres.  
+   * */
+  // Parámetro por defecto con instancias de clase.
+  function receive_a_class($class = new UnaClaseRandom) {
+      echo $class::class;   // muestra una UnaClaseRandom si se envia sin parametros
+  }
+  echo receive_a_class() . PHP_EOL; 
+  echo receive_a_class(new OtraClaseRandom) . PHP_EOL;  // muestra una OtraClaseRandom
+  
+  // Orden de los parámetros
+  function suma($n1, $n2 = 8)
+  {
+      return $n1 + $n2;
+  }
+  
+  echo suma(8) . PHP_EOL;
--- php_poo/clases.php
+++ php_poo/clases.php
@@ -1,9 +1,11 @@
 <?php
-
+/**
+ * Si no hay argumentos para pasar al constructor de la clase, se pueden omitir los paréntesis después del nombre de la clase. 
+ */
 include 'Person.php';
 include 'User.php';
 include 'Admin.php';
 
-$user=new User; // objeto $user
+$user=new User(); // objeto $user
 $user->type=new Admin;  // asigna a la variable de tipo Objeto User un objeto tipo Admin
 echo $user->type->doGreet();    // 'Hola Administrador' accede al metodo doGreet() de la propiedad type del objeto user
