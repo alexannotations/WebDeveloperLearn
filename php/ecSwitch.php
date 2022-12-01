bc749cdcb09ac28e2c6db218482ae741739ec414
alexannotations <69085537+alexannotations@users.noreply.github.com>
2022-12-01 00:01:59 -0600

    estructura match (switch)

--- php/ecSwitch.php
+++ php/ecSwitch.php
@@ -33,4 +33,48 @@ switch( $opcion ){
 
 echo "\n";
 
+# match
+# https://www.php.net/manual/es/control-structures.match.php
+
+function get_country_name_switch($country) {
+
+    $name = "";
+
+    switch ($country) {
+        case 'MX':
+            $name = "México";
+            break;
+
+        case 'COL':
+            $name = "Colombia";
+            break;
+
+        case 'EUA':
+            $name = "Estados Unidos Americanos";
+            break;
+        
+        default:
+            $name = "Lo siento, no conozco ese país";
+            break;
+    }
+
+    return $name;
+    
+}
+
+function get_country_name_match($country) {
+
+    return match($country) {
+        "MX" => "México",
+        "COL" => "Colombia",
+        "EUA" => "Estados Unidos Americanos",
+        default => "Lo siento, no conozco ese país"
+    };
+
+}
+
+echo get_country_name_match("LKASJDKLASDNLAS");
+
+echo "\n";
+
 ?>
