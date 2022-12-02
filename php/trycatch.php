77be21193d1142d0c35fde5623e122b82b94fbf3
alexannotations <69085537+alexannotations@users.noreply.github.com>
2022-12-02 11:51:20 -0600

    metodos de las excepciones

--- php/trycatch.php
+++ php/trycatch.php
@@ -2,6 +2,7 @@
 /** 
  * https://www.php.net/manual/es/language.exceptions.php 
  * https://www.php.net/manual/es/class.throwable.php
+ * https://www.php.net/manual/es/class.exception.php
  * 
  * Errores en tiempo de ejecución
  * php lanza una excepcion, que se manejan con la
@@ -10,7 +11,9 @@
  * Todas las excepciones en php deben implementar la interfaz ```Throwable```.
  * La clase ```Exception``` de php implementa directamente a la interfaz ```Throwable```
  * 
- */
+ * https://github.com/nunomaduro/collision
+ * https://github.com/filp/whoops
+ * */
 
 
 try {
@@ -36,4 +39,39 @@ try {
 } catch (Throwable $e) {
     // solo se ejecuta si se genera el error
     echo $e->getMessage();
+}
+
+/** Métodos de la clase Exception implements Throwable */
+$e->getMessage(); // Mensaje de Excepción
+$e->getCode(); // Código de una excepción
+$e->getFile(); // Obtiene el fichero en el que se creó la excepción
+$e->getLine(); // Obtiene la línea en el que se creó la excepción
+$e->getTrace(); // Obtiene la traza de la pila
+$e->getTraceAsString(); // Obtiene la traza de la pila como una cadena de caracteres
+$e->getPrevious(); // Devuelve la excepcion anterior
+$e->__toString(); // Representa la excepcion en formato de cadena
+
+
+function caminito2() {    return 20 / 0;}
+function caminito() {    return caminito2();}
+function division() {    return caminito();}
+
+try {
+    $resultado = division();
+    echo $resultado;
+} catch (Throwable $e) {
+    // echo $e->getFile();  // .../trycatch.php
+    // echo $e->getLine();
+    // echo $e->getMessage();   // division by zero
+    // echo $e->getCode();  // 0
+
+    /** 
+     * cadena de ejecucion para que el error sucediera
+     * Si sucede dentro del bloque try, no mostrara nada en el array.
+     * pero si sucede fuera del bloque a traves de una funcion, mostrara 
+     * un array con file, code line, function y args. 
+     * O bien mostrara varios arrays, segun el camino, por cada funcion utilizada.
+     * */
+    var_dump($e->getTrace());   
+
 }
